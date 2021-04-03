c1f8e83939839d6dcadf6b9f0767e767a500e1a6
matt <anderson_rayner@hotmail.com>
2021-04-02 22:55:48 -0700

    Added code for automatically creating events with the flight mode

--- ImportFilter/import_px4.m
+++ ImportFilter/import_px4.m
@@ -20,9 +20,9 @@
 
 function fds = import_px4(file)
 
-if file==0
-    warning('Error loading file.')
-    return
+if ~nargin
+    file = 'log_13_2019-9-14-02-08-36.ulg';
+    file = 'log_28_2021-4-2-16-38-26.ulg';
 end
 
 tic
@@ -39,14 +39,14 @@ fds.BoardSupportPackage = 'PX4';
 
 
 %% Convert ulg into csv files
-[path, file, extension] = fileparts(file); ...
-    path = [path,'\'];
+[path, file_root] = fileparts(file); ...
 
-csv_folder = [path,'csv_',file];
-mkdir([path,'csv_',file]);
+csv_folder = fullfile(path,['csv_',file_root]);
+mkdir(csv_folder);
 
 % Convert file to CSV
-evalc(['!ulog2csv -o ',[path,'csv_',file,'\'],' ',[path,file,extension]]);
+cmd = ['!ulog2csv -o ',csv_folder,'\ ',file];
+evalc(cmd);
 
 %% read data
 % Loop through each file in the csv folder
@@ -56,10 +56,14 @@ csv_files = dir([csv_folder,'\**\*.csv']);
 if (isempty(csv_files))
     fprintf('\n\n***************************************\n\n');
     fprintf('pyulog (or python) may not be installed properly.  Check that you can manually convert .ulg files to csv\n\n');
+    fprintf('    (spaces in file names are bad too...)\n');
+    fprintf('\t%s\n',cmd)
     fprintf('\n\n***************************************\n\n');
+    keyboard
 end
 
-t_min = inf;
+t_start = inf;
+t_end = -inf;
 
 for ii = 1:length(csv_files)
     % Print debug stuff
@@ -68,7 +72,7 @@ for ii = 1:length(csv_files)
     fprintf('Found file  << %s >>\n',data_file);
     
     % Get data stream name
-    groupName = csv_files(ii).name(length(file)+2:end-4);
+    groupName = csv_files(ii).name(length(file_root)+2:end-4);
     fprintf('\tImporting field %s\n',groupName);
     
     % Get all the headers and stuff
@@ -93,8 +97,17 @@ for ii = 1:length(csv_files)
         if strcmp(varNames{jj}(end),'_')
             varNames{jj} = varNames{jj}(1:end-1);
         end
+        
+        
             
     end
+    
+    % Convert the time element to seconds
+    DAT(:,1) = DAT(:,1) / 1e6;
+    
+    % Find t_start and t_end
+    t_start = min(t_start,DAT(1,1));
+    t_end   = max(t_end  ,DAT(end,1));
     
     % Add special fields that PX4 doesn't normally have (because it's stupid)
     if strcmp(groupName,'vehicle_attitude_0')
@@ -113,9 +126,6 @@ for ii = 1:length(csv_files)
         quat_angles = [q0, q1, q2, q3];
         euler_angles = q2e(quat_angles)*180.0/pi;
         DAT = [ DAT, euler_angles ];
-        
-        % Take t_min from the attitude data
-         t_min = min(t_min,DAT(1,1));
         
     end
     
@@ -200,14 +210,92 @@ for ii = 1:length(csv_files)
 end
 
 % Remove the csv folder
-rmdir([path,'csv_',file],'s');
+rmdir(csv_folder,'s');
 
-% Correct all the time vectors
-for ii = 1:length(csv_files)
-    fds.fdata{7,ii+1}(:,1) = (fds.fdata{7,ii+1}(:,1) - t_min)/1e6;
-    
+%% Fix up the time so starts at t = 0
+fds.timeOffset = t_start; t_end = t_end - t_start;
+for ii = 2:numel(fds.fdata(1,:))
+    fds.fdata{fds.fdataRows.data,ii}(:,1) = fds.fdata{fds.fdataRows.data,ii}(:,1) - fds.timeOffset;
 end
+    
+%% Add events based on flight mode changes
+% We can use MODE.ModeNum to work this out
+modeTimes   = kVIS_fdsGetChannel(fds, 'manual_control_setpoint_0','Time');
+modeNumbers = kVIS_fdsGetChannel(fds, 'manual_control_setpoint_0','mode_slot');
+% modeReasons = kVIS_fdsGetChannel(fds, 'manual_control_setpoint_0','data_source');
+
+modeTimes(1) = 0; % Force the first mode time to 0
+
+% Combine short and same mode changes
+if numel(modeTimes > 0.5)
+    ii = 2;
+    while ii < numel(modeNumbers)
+        if modeNumbers(ii-1) == modeNumbers(ii)
+            % Remove entry as mode didn't change
+            modeNumbers(ii) = [];
+            modeTimes(ii)   = [];
+            % modeReasons(ii) = [];
+        else
+            ii = ii+1;
+        end
+    end
+end
+
+% Add a marker for the mode at log's end
+modeTimes(end+1) = t_end;
+modeNumbers(end+1) = modeNumbers(end);
+% modeReasons(end+1) = modeReasons(end);
+
+% Loop through modeTimes and store data
+eventNumber = 0;
+
+for ii = 1:numel(modeTimes)-1
+    % Get info
+    t_in  = modeTimes(ii);
+    t_out = modeTimes(ii+1);
+    
+    % Check if change was valid
+    if t_out-t_in > 0.5
+        
+        modeReason = 'Mode Switch';
+        
+        modeType = modes_PX4_Copter(modeNumbers(ii));
+        
+        % Fill out eList
+        eventNumber = eventNumber+1;
+        eList(eventNumber).type = modeType;
+        eList(eventNumber).start= t_in;
+        eList(eventNumber).end  = t_out;
+        eList(eventNumber).description = modeReason;
+        eList(eventNumber).plotDef='';
+    end
+        
+end
+
+% Add eList to eventList
+fds.eventList = eList;
 
 %% Return the fds struct
 return
 
+end
+
+function modeName = modes_PX4_Copter(modeNumber)
+% From https://github.com/PX4/PX4-Autopilot/blob/master/src/modules/commander/px4_custom_mode.h#L45
+
+% These don't seem to match up so who knows what's going on...
+
+switch (modeNumber)
+	case 1; modeName = 'MANUAL';
+	case 2; modeName = 'ALTCTL';
+	case 3; modeName = 'POSCTL';
+	case 4; modeName = 'POSITION';
+	case 5; modeName = 'OFFBOARD';
+	case 6; modeName = 'OFFBOARD';
+	case 7; modeName = 'STABILIZED';
+	case 8; modeName = 'RATTITUDE_LEGACY';
+	case 9; modeName = 'SIMPLE';
+    otherwise; modeName = ['UNKNOWN_(',num2str(modeNumber),')'];
+end
+return
+end
